f436ac099ecfd0fbe0388f16df7a3b57e3ca896d
Stavros Karathanasis <83426703+Stav-Kr@users.noreply.github.com>
2021-07-09 01:59:50 +0100

    creation of the repository

---
+++ WebScrapper2/main.py
@@ -0,0 +1,25 @@
+# scrapes https://metro.co.uk/news/ for discounds displayed on the page
+
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+response = requests.get("https://metro.co.uk/news/")
+soup = BeautifulSoup(response.text, "html.parser")
+discount_class = soup.select(".metro-discount-codes clearfix")
+links = soup.select(".metro-discount-codes-item")
+a_disc = soup.find_all(target="_blank")
+
+
+def discounds(links):
+
+    for item in enumerate(links):
+        
+        anchors = item.find("a")
+        href_from_anchors = anchors.get("href")
+        pprint.pprint(href_from_anchors)
+        # title = links[index].getText()
+        pprint.pprint(href_from_anchors)
+
+
+discounds(links)
