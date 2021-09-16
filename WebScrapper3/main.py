78f2f1e6d76078e4907881e90dce4474dbeb603c
Stavros Karathanasis <83426703+Stav-Kr@users.noreply.github.com>
2021-09-16 01:32:29 +0100

    started the project (not finished)

--- WebScrapper3/main.py
+++ WebScrapper3/main.py
@@ -1,1 +1,29 @@
 # WORK IN PROGRESS ... ...
+
+import requests
+from bs4 import BeautifulSoup
+import pprint
+import lxml
+
+response = requests.get("https://coinmarketcap.com/currencies/ethereum/")
+soup = BeautifulSoup(response.text, "lxml")
+price_class = soup.select(".priceValue")
+divs = soup.find_all('div')
+
+
+#print(divs)
+print(price_class)
+def real_price(divs):
+
+    for item in (divs):
+        
+        anchors = item.find('text')
+        #href_from_anchors = anchors.get("href")
+        #title_from_anchors = anchors.get('title')
+        print(anchors)
+        
+        
+        
+
+
+real_price(price_class)
